2803075c3df4a2e25c26e84606dd7041bd31c518
ruchikageeminda97 <ruchikarg97@gmail.com>
2023-09-02 15:34:31 +0530

    Creating project

---
+++ constants/index.js
@@ -0,0 +1,106 @@
+export const rapidApiKey = '5c5d739548msh5c6ed5237cdcac9p1e4e0ejsn9858cafacffe';
+// below is the api used from rapid api hub
+// https://rapidapi.com/ytdlfree/api/youtube-v3-alternative/
+
+export const categories = [
+  'All',
+  'Gaming',
+  'React Routers',
+  'Music', 
+  'John Snow',
+  'Live', 
+  'Gadgets',
+  'Programming',
+  'Podcasts',
+  'Computers',
+  'Others'
+]
+
+export const shortVideos = [
+  {
+    id: 2,
+    title: 'Reasons you should learn React Native',
+    viewCount: '355k',
+    image: require('../assets/short.jpg')
+  },
+  {
+    id: 2,
+    title: "Mobile app development",
+    viewCount: '132k',
+    image: require('../assets/short.jpg')
+  },
+  {
+    id: 3,
+    title: "Tesla's Wireless Charger: Explained!",
+    viewCount: '2M',
+    image: require('../assets/short.jpg')
+  },
+  {
+    id: 4,
+    title: "Rare Moments in Football 🥶🤩",
+    viewCount: '167k',
+    image: require('../assets/short.jpg')
+  },
+  {
+    id: 5,
+    title: 'Fastest Way to Learn React in 2023 🔥',
+    viewCount: '1.2M',
+    image: require('../assets/short.jpg')
+  },
+  
+
+]
+
+export const videos = [
+  {
+    id: 1,
+    title: 'Awrudu',
+    channelTitle: 'Wasthi',
+    thumbnail: require('../assets/thumb1.jpg'),
+    avatar: require('../assets/prof.png'),
+    viewCount: '130M',
+    duration: '14:50',
+    publishedText: '2 years ago'
+  },
+  {
+    id: 2,
+    title: 'Awrudu',
+    channelTitle: 'Wasthi',
+    thumbnail: require('../assets/thumb2.jpg'),
+    avatar: require('../assets/prof.png'),
+    viewCount: '1M',
+    duration: '4:30',
+    publishedText: '5 months ago'
+  },
+  {
+    id: 3,
+    title: 'Awrudu',
+    channelTitle: 'Wasthi',
+    thumbnail: require('../assets/thumb1.jpg'),
+    avatar: require('../assets/prof.png'),
+    viewCount: '151k',
+    duration: '6:00',
+    publishedText: '1 year ago'
+  },
+  {
+    id: 4,
+    title: 'Awrudu',
+    channelTitle: 'Wasthi',
+    thumbnail: require('../assets/thumb2.jpg'),
+    avatar: require('../assets/prof.png'),
+    viewCount: '400k',
+    duration: '7:45',
+    publishedText: '1 Month ago'
+  },
+  {
+    id: 5,
+    title: 'Awrudu',
+    channelTitle: 'Wasthi',
+    thumbnail: require('../assets/thumb1.jpg'),
+    avatar: require('../assets/prof.png'),
+    viewCount: '5K',
+    duration: '20:45',
+    publishedText: '11 days ago'
+  },
+
+]
